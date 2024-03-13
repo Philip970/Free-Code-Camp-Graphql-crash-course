d0a7a92418225150213dc65c0740053ad6152ea8
unknown <philip.ewane97@gmail.com>
2024-03-13 14:58:27 +0100

    mutations

--- index.js
+++ index.js
@@ -45,6 +45,33 @@ const resolvers = {
       return _db.games.find((g) => g.id === parent.game_id);
     },
   },
+  Mutation: {
+    deleteGame(_, args) {
+      _db.games = _db.games.filter((g) => g.id !== args.id);
+
+      return _db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      _db.games.push(game);
+
+      return game;
+    },
+    updateGame(_, args) {
+      _db.games = _db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+
+        return g;
+      });
+
+      return _db.games.find((g) => g.id === args.id);
+    },
+  },
 };
 
 // Server setup
